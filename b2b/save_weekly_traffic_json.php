8f3e8ef77a1bb514c6681629c46ba64c606cdf2e
Christophe <rollbich2@yahoo.fr>
2022-01-18 22:58:46 +0100

    correctif weekly traffic

--- b2b/save_weekly_traffic_json.php
+++ b2b/save_weekly_traffic_json.php
@@ -18,17 +18,36 @@ $arr = get_date_array($monday_last_week, $monday);
 echo "Year: ".$year."<br>";
 echo "Week number: ".$week_number."<br>";
 
+function get_file($url) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,$url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+	//curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // si pb de certificat SSL force la requête
+	$result = curl_exec($ch);
+    echo "Fichier: ".$url."<br>";
+	$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	echo "HTTP CODE: " . $status_code."<br>";
+	echo curl_error($ch);
+	curl_close($ch);  
+    return [$result, $status_code];
+}
+
 // chemin d'accès à votre fichier JSON
-$file = $year.'-weekly-flights.json';
-// existence
-if (file_exists($file)) {
-    // mettre le contenu du fichier dans une variable
-    $data = file_get_contents("json/".$file); 
-    // décoder le flux JSON
-    $json_year = json_decode($data);
+//$file = "./json/".$year.'-weekly-flights.json';
+$file = "https://lfmm-fmp.fr/b2b/json/".$year.'-weekly-flights.json';
+$data = get_file($file);
+// existence du fichier
+if ($data[1] == 200) {
+    echo "Fichier ".$file." existant"."<br>";
+    //$data = file_get_contents($file); 
+    $json_year = json_decode($data[0]);
+    var_dump($json_year);
+    echo "<br>";
 } else {
+    echo "Fichier ".$file." inexistant"."<br>";
     $json_year = new stdClass();
-    $json_year->year = $year;
+    $json_year->year = intval($year);
     $json_year->est = new stdClass();
     $json_year->west = new stdClass();
     $json_year->cta = new stdClass();
@@ -38,8 +57,9 @@ $json_year->est->$week_number = $arr[1];
 $json_year->west->$week_number = $arr[2];
 $json_year->cta->$week_number = $arr[0];
 
+echo "Nouveau json"."<br>";
 var_dump($json_year);
-write_json($json_year);
+write_json($json_year, $year);
 
 // get all json from file Between 2 Dates non incluse la dernière date
 function get_date_array($dateTime1, $dateTime2) {
@@ -53,9 +73,11 @@ function get_date_array($dateTime1, $dateTime2) {
     $est = 0;
     $west = 0;
     foreach ($period as $key => $value) {
-        $file_name = $value->format('Ymd')."-vols.json";
-        $data = file_get_contents("json/".$file_name);
-        $donnees = json_decode($data);
+        //$file_name = $value->format('Ymd')."-vols.json";
+        $file_name = "https://lfmm-fmp.fr/b2b/json/".$value->format('Ymd')."-vols.json";
+        //$data = file_get_contents("./json/".$file_name);
+        $data = get_file($file_name);
+        $donnees = json_decode($data[0]);
         $cta += intval($donnees->LFMMCTA[2]);
         foreach ($donnees->LFMMFMPE as $value) {
             if ($value[0] == "LFMRAE") $est += intval($value[2]);
@@ -93,12 +115,10 @@ function addDays($datetime) {
     return $datetime;
 }
 
-function write_json($json) {
-	$year = $json->year;
-	$fp = fopen("json/".$year."-weekly-flights.json", 'w');
+function write_json($json, $year) {
+	$fp = fopen(dirname(__FILE__)."/json/".$year."-weekly-flights.json", 'w');
 	fwrite($fp, json_encode($json));
 	fclose($fp);
-	
 }
 
 ?>
