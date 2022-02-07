eeae26e0207d68153a7284861a3ede6bf0e2095c
Christophe <rollbich2@yahoo.fr>
2022-02-07 21:29:43 +0100

    correctif style

--- trafic/index.php
+++ trafic/index.php
@@ -7,7 +7,7 @@
 <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="robots" content="noindex">
-        <title>Regulations</title>
+        <title>Trafic</title>
 		<link rel="icon" href="favicon.ico" />
 		<script type="text/javascript" src="../js/utils.js"></script>
 		<script type="text/javascript" src="../js/list-component.js"></script>
