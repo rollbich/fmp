cb6b70037896f14d6358690877736691d9b17d2c
Christophe <rollbich2@yahoo.fr>
2022-03-18 11:12:13 +0100

    Changement format json

--- js/instruction.js
+++ js/instruction.js
@@ -7,6 +7,8 @@ async function inst(containerId) {
     for (const elem of supp) {
         elem.addEventListener('click', e => {
             const id = elem.dataset.id;
+            const day = elem.dataset.date;
+            const zone = elem.dataset.zone.toLowerCase();
             const parent = elem.parentNode;
             cuteAlert({
                 type: "question",
@@ -17,11 +19,11 @@ async function inst(containerId) {
             }).then((e)=>{
                 if ( e == ("confirm")){
                     let ind;
-                    instr.forEach( (el, index) => {
+                    instr[zone][day].forEach( (el, index) => {
                         if (id == el["id"]) ind = index;
                     })
                     parent.parentNode.removeChild(parent);
-                    instr.splice(ind,1);
+                    instr[zone][day].splice(ind,1);
                     const data = {
                         method: "post",
                         headers: { "Content-Type": "application/json" },
@@ -53,30 +55,36 @@ function affiche(containerId, instr) {
             <tr class="titre"><th class="top_2px left_2px bottom_2px right_1px">Date</th><th class="top_2px bottom_2px right_1px">Début</th><th class="top_2px bottom_2px right_1px">Fin</th><th class="top_2px right_1px bottom_2px">Secteur</th><th class="top_2px bottom_2px right_1px">Type</th><th class="top_2px bottom_2px right_1px">Comment</th><th class="top_2px bottom_2px right_2px">Supprime</th></tr>
         </thead>
         <tbody>`;
-    instr.sort(compare).forEach( (elem, index) => {
-        const debut = elem["debut"];
-        const fin = elem["fin"];
-        const d = elem["date"];
-        const zone = elem["zone"];
-        const type = elem["type"];
-        const comm = elem["comm"];
-        const id = elem["id"];
-        res += `<tr><td>${d}</td><td>${debut}</td><td>${fin}</td><td>${zone}</td><td>${type}</td><td>${comm}</td><td class="supprime" data-id="${id}">x</td></tr>`;
-    });
+    const cles = Object.keys(instr["est"]);
+    console.log("cles "+cles);
+    for (const dat of cles) {
+        instr["est"][dat].forEach(elem => {
+            const debut = elem["debut"];
+            const fin = elem["fin"];
+            const d = elem["date"];
+            const zone = elem["zone"];
+            const type = elem["type"];
+            const comm = elem["comm"];
+            const id = elem["id"];
+            res += `<tr><td>${d}</td><td>${debut}</td><td>${fin}</td><td>${zone}</td><td>${type}</td><td>${comm}</td><td class="supprime" data-zone="${zone}" data-date="${d}" data-id="${id}">x</td></tr>`;
+        });
+    }
     res += '</tbody></table>';
     $(containerId).innerHTML = res;
 } 
 
 async function ajoute(containerId, ajout) {
     let instr = await loadJson("../instruction.json");
-    let id;
-    for(let i=1;i<2000;i++) {
-        const k = instr.every( elem => elem["id"] != i );
+    let id, i = 1;
+    const zone = ajout["zone"].toLowerCase();
+    const day = ajout["date"];
+    for(let i=1;i<99;i++) {
+        const k = instr[zone][day].every( elem => elem["id"] != i );
         if (k == true) { id = i; break; }
     }
     ajout["id"] = id;
-    instr.push(ajout);
-    //affiche(containerId, instr);
+    if (typeof instr[zone][day] === 'undefined') instr[zone][day] = [];
+    instr[zone][day].push(ajout);
     const data = {
         method: "post",
         headers: { "Content-Type": "application/json" },
