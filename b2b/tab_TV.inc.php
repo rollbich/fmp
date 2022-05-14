f33984bca92eb83bbe555c11bb2d7cd76636de5e
Christophe <rollbich2@yahoo.fr>
2022-05-14 15:05:01 +0200

    Add count TV_APP par jour

--- b2b/tab_TV.inc.php
+++ b2b/tab_TV.inc.php
@@ -1,4 +1,6 @@
 <?php
+// Liste des vols des TV à compter par jour
 $tab_TVE = ["LFMRAE", "LFMRAEE", "LFMGYAB", "LFMSBAM", "LFMGY", "LFMAB", "LFMEK"];
 $tab_TVW = ["LFMRAW", "LFMMALY", "LFMWW", "LFMMF", "LFMDZ"];
+$tab_TVAPP = ["LFKJ","LFKF","LFKB","LFKC","LFMN","LFMD","LFTZ","LFTH","LFML","LFMV","LFMQ","LFLL","LFLY","LFLS","LFLB","LFLP","LFLC","LFMT","LFTW","LFMP","LFMU","LFLV","LFLN","LFLU"];
 ?>
