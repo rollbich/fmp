5c24a37f371ee7f0f1a3f315a4a06d05640682a9
Christophe <rollbich2@yahoo.fr>
2022-05-15 10:19:14 +0200

    add small airport

--- b2b/tab_TV.inc.php
+++ b2b/tab_TV.inc.php
@@ -2,5 +2,5 @@
 // Liste des vols des TV à compter par jour
 $tab_TVE = ["LFMRAE", "LFMRAEE", "LFMGYAB", "LFMSBAM", "LFMGY", "LFMAB", "LFMEK"];
 $tab_TVW = ["LFMRAW", "LFMMALY", "LFMWW", "LFMMF", "LFMDZ"];
-$tab_TVAPP = ["LFKJ","LFKF","LFKB","LFKC","LFMN","LFMD","LFTZ","LFTH","LFML","LFMV","LFMQ","LFLL","LFLY","LFLS","LFLB","LFLP","LFLC","LFMT","LFTW","LFMP","LFMU","LFLV","LFLN","LFLU"];
+$tab_TVAPP = ["LFKJ","LFKF","LFKB","LFKC","LFMN","LFMD","LFTZ","LFTH","LFML","LFMV","LFMQ","LFLL","LFLY","LFLS","LFLB","LFLP","LFLC","LFMT","LFTW","LFMP","LFMU","LFLV","LFLN","LFLU","LFMH","LFMI","LFMY","LFLI","LFMC","LFMF","LFTF","LFMA"];
 ?>
