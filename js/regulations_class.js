f010722c8e76b42041d5e5e5ac610454b1486fa0
Christophe <rollbich2@yahoo.fr>
2022-05-16 21:29:44 +0200

    Add affichage app flights

--- js/regulations_class.js
+++ js/regulations_class.js
@@ -1,3 +1,14 @@
+/*  ---------------------
+	  tableau des TVset
+	--------------------- */
+const lfmm_tvset = ["LFMMFMPE", "LFMMFMPW", "LFMMAPP"];
+const lfbb_tvset = ["LFBBFMP", "LFBBAPP"];
+const lfee_tvset = ["LFEEFMP", "LFEEAPP"];
+const lfff_tvset = ["LFFFFMPE", "LFFFFMPW", "LFFFAD"];
+const lfrr_tvset = ["LFRRFMP", "LFRRAPP"];
+const dsna_tvset = ["LFDSNA"];
+
+
 class regul {
 	constructor(day, zone) {
 		this.day = day;
@@ -6,18 +17,15 @@ class regul {
 
 	async init() {
 		this.regul = await this.get_regul();
-		/* 
-		---------------------
-		tableau des TVset
-		--------------------- 
-		*/
-        
-		this.lfmm_tvset = ["LFMMFMPE", "LFMMFMPW", "LFMMAPP"];
-		this.lfbb_tvset = ["LFBBFMP", "LFBBAPP"];
-		this.lfee_tvset = ["LFEEFMP", "LFEEAPP"];
-		this.lfff_tvset = ["LFFFFMPE", "LFFFFMPW", "LFFFAD"];
-		this.lfrr_tvset = ["LFRRFMP", "LFRRAPP"];
-		this.dsna_tvset = ["LFDSNA"];
+	
+		lfmm_tvset.forEach(tvset => {
+			this[tvset] = {};
+		})
+
+		lfmm_tvset.forEach(tvset => {
+			this[tvset].tot_delay = this.get_total_delay(tvset);
+			this[tvset].delay_by_cause = this.get_reg_by_causes(tvset);
+		})
 	}
 
 	/*  -------------------------------------------------------------
@@ -33,18 +41,36 @@ class regul {
 		return resp;
 	}
 
-	async get_total_delay() {
-		let total_delay_est = 0, total_delay_west = 0, total_delay_app = 0; 
-		this.regul["LFMMFMPE"].forEach( value => {
-			total_delay_est += value.delay;
+	get_total_delay(tvset) {
+		let total_delay = 0;
+		this.regul[tvset].forEach( value => {
+			total_delay += value.delay;
 		});
-		this.regul["LFMMFMPW"].forEach( value => {
-			total_delay_west += value.delay;
+		return total_delay;
+	}
+
+	/*  --------------------------
+		  {
+			  "ATC_STAFFING": 318,
+			  "WEATHER": 728
+		  }
+		-------------------------- */
+	get_reg_by_causes(tvset) {
+		const reg = {};
+		let causes = [];
+		this.regul[tvset].forEach( value => {
+			causes.push(value.reason);
 		});
-		this.regul["LFMMAPP"].forEach( value => {
-			total_delay_app += value.delay;
+		// enlève les doublons
+		causes = [...new Set(causes)];
+		causes.forEach(value => {
+			reg[value] = 0;
+		})
+		this.regul[tvset].forEach( value => {
+			reg[value] += value.delay;
 		});
-		return {"est": total_delay_est, "west": total_delay_west, "app": total_delay_app}
+		console.log(reg);
+		return reg;
 	}
 
 }
@@ -71,6 +97,7 @@ class period_regul {
 	-------------------------------------------------------------------------- */
     async init() {
 		this.reguls = await this.get_reguls();
+		this.tot_delays = this.get_total_period_delay();
 		console.log(this.reguls);
 	}
 
@@ -79,69 +106,121 @@ class period_regul {
         for (const date of this.dates_arr) {
              const r = new regul(date, this.zone);
 			 await r.init();
-             reguls[date] = r.regul;
+             reguls[date] = {
+				 "reguls": r.regul,
+				 "LFMMFMPE": r["LFMMFMPE"],
+				 "LFMMFMPW": r["LFMMFMPW"],
+				 "LFMMAPP": r["LFMMAPP"]
+			 }
         }
 		return reguls;
+	}
+
+	get_total_period_delay() {
+		const delay = {"LFMMFMPE": 0, "LFMMFMPW": 0, "LFMMAPP": 0}
+		for (const date of this.dates_arr) {
+			delay["LFMMFMPE"] += this.reguls[date]["LFMMFMPE"]["tot_delay"];
+			delay["LFMMFMPW"] += this.reguls[date]["LFMMFMPW"]["tot_delay"];
+			delay["LFMMAPP"] += this.reguls[date]["LFMMAPP"]["tot_delay"];
+		}
+		return delay;
+	}
+	/*  ------------------------------------------
+			delay = {
+				"LFMMFMPE": {
+					"cause1": 41,
+					"cause2": 1400
+				}, 
+				...
+			}
+		------------------------------------------ */
+	get_total_period_delay_by_cause() {
+		let delay = {"LFMMFMPE": {}, "LFMMFMPW": {}, "LFMMAPP": {}};
+		let tab_FMPE = [];
+		let tab_FMPW = [];
+		let tab_APP = [];
+		for (const date of this.dates_arr) {
+			tab_FMPE = [...tab, ...Object.keys(this.reguls[date]["LFMMFMPE"]["delay_by_cause"])];
+			tab_FMPW = [...tab, ...Object.keys(this.reguls[date]["LFMMFMPW"]["delay_by_cause"])];
+			tab_APP = [...tab, ...Object.keys(this.reguls[date]["LFMMAPP"]["delay_by_cause"])];
+		}
+		tab_FMPE.forEach(cause => {
+			delay["LFMMFMPE"][cause] = 0;
+		})
+		tab_FMPW.forEach(cause => {
+			delay["LFMMFMPW"][cause] = 0;
+		})
+		tab_APP.forEach(cause => {
+			delay["LFMMAPP"][cause] = 0;
+		})
+		for (const date of this.dates_arr) {
+			Object.keys(this.reguls[date]["LFMMFMPE"]["delay_by_cause"]).forEach(cause => {
+				delay["LFMMFMPE"][cause] += this.reguls[date]["LFMMFMPE"]["delay_by_cause"][cause];
+			})
+			Object.keys(this.reguls[date]["LFMMFMPE"]["delay_by_cause"]).forEach(cause => {
+				delay["LFMMFMPW"][cause] += this.reguls[date]["LFMMFMPW"]["delay_by_cause"][cause];
+			})
+			Object.keys(this.reguls[date]["LFMMAPP"]["delay_by_cause"]).forEach(cause => {
+				delay["LFMMAPP"][cause] += this.reguls[date]["LFMMAPP"]["delay_by_cause"][cause];
+			})
+		}
+		return delay;
 	}
 
 	/*  ------------------------------------------------------------------	
 		 @param {string} containerId - Id de l'HTML Element container 
 		------------------------------------------------------------------ */	 
         
-	async show_result_reg(containerId) {
+	show_result_reg(containerId) {
 		let delays = `<h2>Delay : ${reverse_date(this.start_day)} au ${reverse_date(this.end_day)}</h2><br>`;
 		delays += "<div class='delay'>";
 		let res = `
-		<table class="regulation sortable">
+		<table class="sortable">
 			<caption>LFMM-EST : ${reverse_date(this.start_day)} au ${reverse_date(this.end_day)}</caption>
 			<thead><tr class="titre"><th class="space">Date</th><th>Regul Id</th><th>TV</th><th>Début</th><th>Fin</th><th>Délai</th><th>Raison</th><th>vols</th></tr></thead>
 			<tbody>`;
-		let total_delay_est = 0, total_delay_west = 0, total_delay_app = 0; 
+		
         for (const date of this.dates_arr) {
-            this.reguls[date]["LFMMFMPE"].forEach( value => {
+            this.reguls[date]["reguls"]["LFMMFMPE"].forEach( value => {
                 let deb = extract_time(value.applicability.wef);
                 let fin = extract_time(value.applicability.unt);
                 res += '<tr>'; 
                 res +=`<td>${reverse_date(date)}</td><td>${value.regId}</td><td>${value.tv}</td><td>${deb} TU</td><td>${fin} TU</td><td>${value.delay}</td><td>${value.reason}</td><td>${value.impactedFlights}</td>`;
                 res += '</tr>';	
-                total_delay_est += value.delay;
             });
         }
         res += '</tbody></table>';
 		res += `
-		<table class="regulation sortable">
+		<table class="sortable">
 			<caption>LFMM-OUEST : ${reverse_date(this.start_day)} au ${reverse_date(this.end_day)}</caption>
 			<thead><tr class="titre"><th class="space">Date</th><th>Regul Id</th><th>TV</th><th>Début</th><th>Fin</th><th>Délai</th><th>Raison</th><th>vols</th></tr></thead>
 			<tbody>`;
         for (const date of this.dates_arr) {
-            this.reguls[date]["LFMMFMPW"].forEach( value => {
+            this.reguls[date]["reguls"]["LFMMFMPW"].forEach( value => {
                 let deb = extract_time(value.applicability.wef);
                 let fin = extract_time(value.applicability.unt);
                 res += '<tr>'; 
                 res +=`<td>${reverse_date(date)}</td><td>${value.regId}</td><td>${value.tv}</td><td>${deb} TU</td><td>${fin} TU</td><td>${value.delay}</td><td>${value.reason}</td><td>${value.impactedFlights}</td>`;
                 res += '</tr>';	
-                total_delay_west += value.delay;
             });
         }
         res += '</tbody></table>';
 		res += `
-		<table class="regulation sortable">
+		<table class="sortable">
 			<caption>LFMM-APP : ${reverse_date(this.start_day)} au ${reverse_date(this.end_day)}</caption>
 			<thead><tr class="titre"><th class="space">Date</th><th>Regul Id</th><th>TV</th><th>Début</th><th>Fin</th><th>Délai</th><th>Raison</th><th>vols</th></tr></thead>
 			<tbody>`;
         for (const date of this.dates_arr) {
-            this.reguls[date]["LFMMAPP"].forEach( value => {
+            this.reguls[date]["reguls"]["LFMMAPP"].forEach( value => {
                 let deb = extract_time(value.applicability.wef);
                 let fin = extract_time(value.applicability.unt);
                 res += '<tr>'; 
                 res +=`<td>${reverse_date(date)}</td><td>${value.regId}</td><td>${value.tv}</td><td>${deb} TU</td><td>${fin} TU</td><td>${value.delay}</td><td>${value.reason}</td><td>${value.impactedFlights}</td>`;
                 res += '</tr>';	
-                total_delay_app += value.delay;
             });
         }
 		res += '</tbody></table>';
-		
-		delays += `<span class="rect">LFMM CTA : ${total_delay_est+total_delay_west} mn</span><span class="rect">LFMM Est : ${total_delay_est} mn</span><span class="rect">LFMM West : ${total_delay_west} mn</span><span class="rect">LFMM App : ${total_delay_app} mn</span>`;
+		delays += `<span class="rect">LFMM CTA : ${this.tot_delays["LFMMFMPE"]+this.tot_delays["LFMMFMPW"]} mn</span><span class="rect">LFMM Est : ${this.tot_delays["LFMMFMPE"]} mn</span><span class="rect">LFMM West : ${this.tot_delays["LFMMFMPW"]} mn</span><span class="rect">LFMM App : ${this.tot_delays["LFMMAPP"]} mn</span>`;
 		delays += "</div>";
 		delays += res;
 		$(containerId).innerHTML = delays;
