dac15ad9dbfe18775c1b04937034ae996a96aefe
Christophe <rollbich2@yahoo.fr>
2022-06-17 12:44:37 +0200

    Correctif cache

--- aide/index.php
+++ aide/index.php
@@ -37,7 +37,8 @@ require("../php/check_ok.inc.php");
         <img src="photo-cache.jpg" alt="logo firefox">
         <div class="content">
             <h2><a href="cache.php">Inhiber le cache Firefox</a></h2>
-            <p>A faire en cas de mise à jour du site pour être sûr d'avoir la nouvelle version</p>
+            <p>A faire en cas de doute sur une page web qui ne semble pas à jour</p>
+            <p>Sur le site lfmm-fmp.fr, le cache est désactivé et donc les pages sont à jour</p>
             <p>Vendredi 17 juin 2022</p>
         </div>
     </div>
