613a71a2a9cbb4beddbb9d0bf55739a4dae2107c
Christophe <rollbich2@yahoo.fr>
2022-06-29 15:39:41 +0200

    Correctif heure récup réguls

--- b2b/hour_config-fmp.inc.php
+++ b2b/hour_config-fmp.inc.php
@@ -1,4 +1,9 @@
 <?php
+// --------------------------------------------
+//			Serveur OVH lfmm-fmp.fr
+//			Tâche CRON 01:55 local
+// --------------------------------------------
+
 // dernier dimanche d'octobre => heure d'hiver
 $date_hiver = new DateTime('2021-10-31');
 $date_ete = new DateTime('2022-03-27');
@@ -14,17 +19,17 @@ $dat = new DateTime();
 // en php pas scope dans if
 if ($dat < $date_hiver || $dat >= $date_ete) {
 	echo "Heure ete<br/>";
-	// Plage horaire de récupération du H20 et Occ (6h loc à minuit loc)
-	$wef_counts = gmdate('Y-m-d H:i', strtotime("today 06:00"));
-	$unt_counts = gmdate('Y-m-d H:i', strtotime("today 23:59"));
+	// Plage horaire de récupération du H20 et Occ (6h loc à 2h loc)
+	$wef_counts = gmdate('Y-m-d H:i', strtotime("yesterday 06:00"));
+	$unt_counts = gmdate('Y-m-d H:i', strtotime("today 00:00"));
 
 	// Plage horaire de récupération des reguls (minuit UTC à minuit UTC)
-	$wef_regs = gmdate('Y-m-d H:i', strtotime("today 02:00"));
-	$unt_regs = gmdate('Y-m-d H:i', strtotime("tomorrow 01:59"));
+	$wef_regs = gmdate('Y-m-d H:i', strtotime("yesterday 02:00"));
+	$unt_regs = gmdate('Y-m-d H:i', strtotime("today 01:59"));
 
 	// Plage horaire de récupération du nombre de vol par TV (minuit UTC à minuit UTC)
-	$wef_flights = gmdate('Y-m-d H:i', strtotime("today 02:00"));
-	$unt_flights = gmdate('Y-m-d H:i', strtotime("tomorrow 01:59"));
+	$wef_flights = gmdate('Y-m-d H:i', strtotime("yesterday 02:00"));
+	$unt_flights = gmdate('Y-m-d H:i', strtotime("today 01:59"));
 } else {
 	echo "Heure hiver<br/>";
 	// Plage horaire de récupération du H20 et Occ (6h loc à minuit loc)
