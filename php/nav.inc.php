834ed28da305e9d22b8a2c1642e491b98bb04241
Christophe <rollbich2@yahoo.fr>
2022-06-30 22:32:17 +0200

    Correctif lien logo

--- php/nav.inc.php
+++ php/nav.inc.php
@@ -1,6 +1,6 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
 	<div class="navbar-brand">
-		<a class="navbar-item" href="https://bulma.io">
+		<a class="navbar-item" href="https://lfmm-fmp.fr">
       <img src="../images/FMP.png" alt="Logo" width="112" height="28">
     </a>
 
