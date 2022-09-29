77a4f4d0fb52c0d6547dc329d63fd829395839af
Christophe <rollbich2@yahoo.fr>
2022-09-29 22:13:33 +0200

    Add fichier h20 occ journée

--- php/get_h20-occ_lfmm.php
+++ php/get_h20-occ_lfmm.php
@@ -5,6 +5,7 @@
 		@param {string} - type = "H20" ou "Occ"
 	    @param {string} - zone = "est" ou "west"
 	    @param {string} - date = 2022-01-24 
+		@param {string} - heure = 1320 (13h20)
    ---------------------------------------------------------------------------------------------------------------------------------------- */
 function clean($data) {
     $data = trim($data);
@@ -12,10 +13,10 @@ function clean($data) {
     return $data;
 }
 
-function get_courbe($type, $zone, $year, $month, $day) {
+function get_courbe($type, $zone, $year, $month, $day, $heure) {
 	//$login = 'olafatco_tds_lfmm-bf';
 	//$password = '(dgac2021)';
-	$url = "https://lfmm-fmp.fr/b2b/json/$year/$month/$year$month$day-$type-$zone.json";
+	$url = "https://lfmm-fmp.fr/b2b/json/$year/$month/$year$month$day-$type-$zone$heure.json";
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL,$url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
@@ -50,14 +51,19 @@ function get_courbe($type, $zone, $year, $month, $day) {
 	---------------------------------------------------- */
 try {
 	if (isset($_GET["type"]) && isset($_GET["zone"]) && isset($_GET["date"])) {
+		if (isset($_GET["heure"])) {
+			$heure = clean($_GET["heure"]);
+		} else {
+			$heure = "";
+		}
 		$ok_type = true;
 		$ok_zone = true;
 		$ok_date = true;
 		$type = clean($_GET["type"]);
 		$zone = clean($_GET["zone"]);
 		$date = clean($_GET["date"]);
-		if ($type !== "H20" && $type != "Occ") $ok_type = false;
-		if ($zone !== "est" && $zone !== "west") $ok_zone = false;
+		if (strtolower($type) !== "h20" && strtolower($type) != "occ") $ok_type = false;
+		if (strtolower($zone) !== "est" && strtolower($zone) !== "west") $ok_zone = false;
 		$d = explode("-", $date);
 		$year = $d[0];
 		$month = $d[1];
@@ -66,7 +72,7 @@ try {
 		if ($ok_type === false) { throw new Exception("Le type est H20 ou Occ: actuellement $type"); }
 		if ($ok_zone === false) { throw new Exception("La zone est est ou west: actuellement $zone"); }
 		if ($ok_date === false) { throw new Exception("La date est incorrecte: actuellement $date"); }
-		$resultat = get_courbe($type, $zone, $year, $month, $day);
+		$resultat = get_courbe($type, $zone, $year, $month, $day, $heure);
 		header('content-type:application/json');
 		echo $resultat;  
 	} else {
