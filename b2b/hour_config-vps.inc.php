2ff600f49288b2b73ab5c02be13384a7b552ac14
Christophe <rollbich2@yahoo.fr>
2023-03-28 20:45:30 +0200

    Corrige echo

--- b2b/hour_config-vps.inc.php
+++ b2b/hour_config-vps.inc.php
@@ -45,7 +45,7 @@ function get_hiver() {
 	$inter = new DateInterval($requete);
 	$d->sub($inter)->format('Y-m-d');
 	//echo "Dernier dimanche octobre : <br>";
-	//echo $d->sub($inter)->format('Y-m-d');
+	//echo $d;
 	//echo "<br><br>";
 	return $d;
 }
@@ -61,7 +61,7 @@ function get_ete() {
 	$inter = new DateInterval($requete);
 	$d->sub($inter)->format('Y-m-d');
 	//echo "Dernier dimanche mars : <br>";
-	//echo $d->sub($inter)->format('Y-m-d');
+	//echo $d;
 	//echo "<br><br>";
 	return $d;
 }
