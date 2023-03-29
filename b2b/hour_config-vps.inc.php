395c58a0f33115978e672bfd2b98369e992ba3bf
Christophe <rollbich2@yahoo.fr>
2023-03-29 20:50:11 +0200

    Correctif heure été

--- b2b/hour_config-vps.inc.php
+++ b2b/hour_config-vps.inc.php
@@ -7,7 +7,7 @@ $dat = new DateTime();
 // On donne l'heure du serveur et on récupère $wef_counts, regs, flights et $unt_counts, regs, flights en UTC
 // en php pas scope dans if
 if ($dat <= $date_ete || $dat > $date_hiver) {
-	//echo "Heure hiver<br/>";
+	echo "Heure hiver<br/>";
 	// Plage horaire de récupération du H20 et Occ (6h loc à minuit loc)
 	$wef_counts = gmdate('Y-m-d H:i', strtotime("today 05:00"));
 	$unt_counts = gmdate('Y-m-d H:i', strtotime("today 22:59"));
@@ -20,19 +20,23 @@ if ($dat <= $date_ete || $dat > $date_hiver) {
 	$wef_flights = gmdate('Y-m-d H:i', strtotime("today 00:00"));
 	$unt_flights = gmdate('Y-m-d H:i', strtotime("today 23:59"));
 } else {
-	//echo "Heure ete<br/>";
+	echo "Heure ete<br/>";
 	// Plage horaire de récupération du H20 et Occ (6h loc à 2h loc)
-	$wef_counts = gmdate('Y-m-d H:i', strtotime("yesterday 06:00"));
-	$unt_counts = gmdate('Y-m-d H:i', strtotime("today 00:01"));
+	$wef_counts = gmdate('Y-m-d H:i', strtotime("today 04:00"));
+	$unt_counts = gmdate('Y-m-d H:i', strtotime("today 21:59"));
 
 	// Plage horaire de récupération des reguls (minuit UTC à minuit UTC)
-	$wef_regs = gmdate('Y-m-d H:i', strtotime("yesterday 02:00"));
-	$unt_regs = gmdate('Y-m-d H:i', strtotime("today 01:59"));
+	$wef_regs = gmdate('Y-m-d H:i', strtotime("today 00:00"));
+	$unt_regs = gmdate('Y-m-d H:i', strtotime("today 23:59"));
 
 	// Plage horaire de récupération du nombre de vol par TV (minuit UTC à minuit UTC)
-	$wef_flights = gmdate('Y-m-d H:i', strtotime("yesterday 02:00"));
-	$unt_flights = gmdate('Y-m-d H:i', strtotime("today 01:59"));
+	$wef_flights = gmdate('Y-m-d H:i', strtotime("today 00:00"));
+	$unt_flights = gmdate('Y-m-d H:i', strtotime("today 23:59"));
 }
+
+echo "wef/unt counts: $wef_counts - $unt_counts<br>";
+echo "wef/unt regs: $wef_regs - $unt_regs<br>";
+echo "wef/unt flights: $wef_flights - $unt_flights<br>";
 
 // 	---------------------------------------------------------
 // 		@return (DateTime) jour de passage à l'heure hiver
