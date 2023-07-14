bdc7f8129ea03a78be1c4a92aff9de688318c3ee
Christophe <rollbich2@yahoo.fr>
2023-07-14 14:29:38 +0200

    Ajout de ZL

--- b2b/write_mv_otmv.php
+++ b2b/write_mv_otmv.php
@@ -39,7 +39,7 @@ function lfm(String $elem) {
 
 $tv_est = array_map("lfm", ["RAE", "RAEM", "RAES", "RAEE", "RAEE1", "SBAM", "MNST", "BTAJ", "SAB", "BAM", "SBM", "MN", "ST", "AJ", "BT", "EK", "EK1", "EK2", "EK3", "EK12", "EK3", "EK23", "E12", "E3", "E23", "KK", "K12", "K3", "K23", "EE", "KK", "E1", "E2", "K1", "K2", "AB", "AB1", "AB2", "AB3", "AB4", "AB12", "AB34", "B12", "B34", "B1", "B2", "B3", "B4", "AA", "BB", "A12", "A34", "A1", "A2", "A3", "A4", "GY", "GY1", "GY2", "GY3", "GY4", "GY12", "GY34", "GG", "YY", "Y12", "Y34", "Y1", "Y2", "Y3", "Y4", "G12", "G34", "G1", "G2", "G3", "G4", "GYAB", "GYA"]);
 
-$tv_west = array_map("lfm", ["RAW", "RAWM", "RAWN", "RAWS", "WLMO", "MALY", "LYO", "MOLYO", "MML", "LE", "LOLS", "LELS", "LOLE", "LS", "LO", "MO", "ML", "MOML", "W1", "W23", "W12", "W2", "W3", "WM", "WW", "WMF", "MF", "MF1", "MF2", "MF3", "MF4", "MM", "M12", "M1", "M123", "M34", "M234", "M2", "M3", "M4", "FF", "F12", "F1", "F2", "MF12", "F123", "MF34", "F234", "F34", "F3", "F4", "DZ", "DD", "ZZ", "DH", "ZH", "DL", "DZL", "DZH", "FDZ", "MFDZ"]);
+$tv_west = array_map("lfm", ["RAW", "RAWM", "RAWN", "RAWS", "WLMO", "MALY", "LYO", "MOLYO", "MML", "LE", "LOLS", "LELS", "LOLE", "LS", "LO", "MO", "ML", "MOML", "W1", "W23", "W12", "W2", "W3", "WM", "WW", "WMF", "MF", "MF1", "MF2", "MF3", "MF4", "MM", "M12", "M1", "M123", "M34", "M234", "M2", "M3", "M4", "FF", "F12", "F1", "F2", "MF12", "F123", "MF34", "F234", "F34", "F3", "F4", "DZ", "DD", "ZZ", "DH", "ZH", "DL", "ZL", "DZL", "DZH", "FDZ", "MFDZ"]);
 
 
 function get_mv_otmv($plan) {
@@ -48,7 +48,7 @@ function get_mv_otmv($plan) {
     $soapClient = new B2B();
     $result = new stdClass();
     $result->MV = $soapClient->flowServices()->get_easy_capacity_plan($plan, $d);
-    $result->OTMV = $soapClient->flowServices()->get_otmv_plan($plan, $d);
+    $result->OTMV = $soapClient->flowServices()->get_otmv_plan($plan, $d, $soapClient->getCurrentVersion());
     return $result;
 }
 
