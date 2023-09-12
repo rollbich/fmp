ac6488bbd932e596fa1f2efd2416b848906dfd8d
rollbich <93681690+rollbich@users.noreply.github.com>
2023-09-12 14:13:00 +0200

    change gitignore

--- js/base.js
+++
@@ -1,6 +0,0 @@
-/*  ---------------------------------------------------------------
-       dossier de base
-       utilisé uniquement dans read_schema_realisé (ouverture.js)
-    --------------------------------------------------------------- */
-const dir = "fmp/";
-
