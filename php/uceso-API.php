da08592e685bf4a2944a8dceef769ba3126ea687
rollbich <93681690+rollbich@users.noreply.github.com>
2023-09-19 16:32:30 +0200

    Correctif api uceso

--- php/uceso-API.php
+++ php/uceso-API.php
@@ -19,10 +19,82 @@ require_once("capa_class.php");
             "uceso" : nombre d'ucecos par 15 minutes [ ["00:00", ucesos], [...] ...]
             "compacted_uceso" : nombre d'ucesos [ ["00:00", "02:30", ucesos], ...]
             "pc_total_horsInstrRD_15mn" : PC hors instruction et RD bleus mais avec JX et RD JX
-            "pc_instr_15mn" : PC instruction par 15 minutes
+            "pc_total_instr_15mn" : PC instruction par 15 minutes
             "pc_RD_15mn" : RD bleus hors JX {"RDS1b-ete" : [ ["00:00", nb_pc], ...], ...}
             "pc_total_RD_15mn" : total RD bleus par 15 minutes
-            "pc_vac" : données OLAF
+            "workingTeam" : { "J2": 1, "vac": equipe, ...}
+            "pc_vac" : données OLAF remaniées
+                {"vac" : {
+                    equipe: n° equipe
+                    nbcds: nombre,
+                    nbpc: nombre,
+                    BV: nombre,
+                    RO: nombre,
+                    ROinduit: nombre,
+                    RoList: ["prenom nom", "..."]
+                    CDS : "prenom nom",
+                    renfort: nombre ( = recyclage non bleu),
+                    renfortAgent : {
+                        "prenom nom": {
+                        nom: nom,
+                        prenom: nom,
+                        nomComplet: "prenom nom",
+                        role: [numéro, ..., ...],
+                        fonction: "PC-DET"
+                    }
+                    RPL: {"nom remplaçant" : "nom remplacé"},
+                    stage: ["nom", "..."],
+                    conge: ["nom", "..."],
+                    teamNominalList : {
+                        agentsList: ["nom", "...", ...],
+                        "prenom nom": {
+                            nom: nom,
+                            prenom: nom,
+                            nomComplet: "prenom nom",
+                            role: [numéro, ..., ...]
+                        },...
+                    teamToday: {
+                        "prenom nom": {
+                            nom: nom,
+                            prenom: nom,
+                            nomComplet: "prenom nom",
+                            role: [numéro, ..., ...],
+                            fonction: "CDS" ou "PC-CDS" ou "ACDS" ou "PC" ou "stagiaire",
+                        },...
+                        inclut les gens de l'équipe + les remplaçants mais pas les pc détachés
+                    },
+                    workingTeam: {
+                        "vac": n° equipe,
+                        ...
+                    }
+                    roles: {
+                        CDS: 82,
+                        ACDS: 80,
+                        DET: 14,
+                        ASS_SUB: 37,
+                        STAGIAIRE: 10,
+                        EXP_OPS: 145,
+                        PC_MU: 98,
+                        PC_salle: 9,
+                        CE: 25
+                    },
+                    RD: {
+                        "RDJ1-ete": {
+                            nombre: nombre,
+                            nombre_det: nombre,
+                            agent: {
+                                "prenom nom": "détaché"
+                            }
+                        },
+                        "JXB-ete": {
+                            ...
+                        }
+                    },
+                    TDS_Supp: {
+                        = RD sans les JX
+                    }
+                    }
+                }}
         }
 /*  -------------------------------------------------------------------------------------------- */
 
