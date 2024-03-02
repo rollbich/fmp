74c71f2fbd47aa7ff11ad896ea7614e8ea86bf67
rollbich <93681690+rollbich@users.noreply.github.com>
2024-03-02 14:30:04 +0100

    Correctif erreur PHP 404

--- php/get_rea_4f.php
+++ php/get_rea_4f.php
@@ -22,45 +22,52 @@ header('content-type:application/json');
 
 function get_rea_4f($year, $month, $day, $zone) {
 	$fichier = "../Realise/$year/$month/$year$month$day"."_000000_LFMM-$zone.xml";
-    $contenu = simplexml_load_file($fichier);
-      
-    $res = new stdClass();
-    $res->date = "$day-$month-$year";
-    $res->zone = $zone;
-    $res->mapping = [];
-    
-    foreach ($contenu->mapping as $mapping) {
-        $heure_debut = substr($mapping->attributes()['time'], 11, 5);
-        $obj = new StdClass();
-        $obj->start_time = $heure_debut;
-        $obj->pos = [];
+    if (file_exists($fichier)) {
+        $contenu = simplexml_load_file($fichier);
+        
+        $res = new stdClass();
+        $res->date = "$day-$month-$year";
+        $res->zone = $zone;
+        $res->mapping = [];
+        
+        foreach ($contenu->mapping as $mapping) {
+            $heure_debut = substr($mapping->attributes()['time'], 11, 5);
+            $obj = new StdClass();
+            $obj->start_time = $heure_debut;
+            $obj->pos = [];
 
-        $nbr_pos = count($mapping->pos);
-        $objet = new StdClass();
-        
-        $objet->pos_name = substr($mapping->pos[0]->attributes()['name'], 0, 3);
-        $objet->pos_resps = (string) $mapping->pos[0]->attributes()['resps'];
-        $objet->pos_regroup = (string) $mapping->pos[0]->attributes()['regroup'];
-        // S'il n'y a pas d'espace dans la chaine resps alors le secteur est élémentaire
-        // On remplace regroup qui est vide par resps qui contient le secteur élémentaire
-        if (strpos($objet->pos_resps, " ") === FALSE) $objet->pos_regroup = $objet->pos_resps; 
-        array_push($obj->pos, $objet);
-        
-        for($i=0;$i<$nbr_pos-1;$i++) {
+            $nbr_pos = count($mapping->pos);
             $objet = new StdClass();
-            $objet->pos_name = substr($mapping->pos[$i+1]->attributes()['name'], 0, 3);
-            $objet->pos_resps = (string) $mapping->pos[$i+1]->attributes()['resps'];
-            $objet->pos_regroup = (string) $mapping->pos[$i+1]->attributes()['regroup'];
-            // on push que si c'est une nouvelle position
-            if (strcmp($objet->pos_name, substr($mapping->pos[$i]->attributes()['name'],0,3)) != 0) {
-                if (strpos($objet->pos_resps, " ") === FALSE) $objet->pos_regroup = $objet->pos_resps; 
-                array_push($obj->pos, $objet);
+            
+            $objet->pos_name = substr($mapping->pos[0]->attributes()['name'], 0, 3);
+            $objet->pos_resps = (string) $mapping->pos[0]->attributes()['resps'];
+            $objet->pos_regroup = (string) $mapping->pos[0]->attributes()['regroup'];
+            // S'il n'y a pas d'espace dans la chaine resps alors le secteur est élémentaire
+            // On remplace regroup qui est vide par resps qui contient le secteur élémentaire
+            if (strpos($objet->pos_resps, " ") === FALSE) $objet->pos_regroup = $objet->pos_resps; 
+            array_push($obj->pos, $objet);
+            
+            for($i=0;$i<$nbr_pos-1;$i++) {
+                $objet = new StdClass();
+                $objet->pos_name = substr($mapping->pos[$i+1]->attributes()['name'], 0, 3);
+                $objet->pos_resps = (string) $mapping->pos[$i+1]->attributes()['resps'];
+                $objet->pos_regroup = (string) $mapping->pos[$i+1]->attributes()['regroup'];
+                // on push que si c'est une nouvelle position
+                if (strcmp($objet->pos_name, substr($mapping->pos[$i]->attributes()['name'],0,3)) != 0) {
+                    if (strpos($objet->pos_resps, " ") === FALSE) $objet->pos_regroup = $objet->pos_resps; 
+                    array_push($obj->pos, $objet);
+                }
             }
+            array_push($res->mapping, $obj);
         }
-        array_push($res->mapping, $obj);
+        return $res;
+
+    } else {
+        $res = new stdClass();
+        $res->error = "fichier manquant";
+        return $res;
     }
     
-    return $res;
 }
 
 $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
