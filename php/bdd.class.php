186bcfd853e535190ca8a7060991fbaeef0cbd6b
rollbich <93681690+rollbich@users.noreply.github.com>
2024-03-08 15:13:41 +0100

    remove echo

--- php/bdd.class.php
+++ php/bdd.class.php
@@ -678,7 +678,6 @@ class bdd {
     // JSON_COMPACT(json_string)
     // $tvset : LFMMFMPE, LFMMFMPW, LFMMAPP, LFMMAPPE, LFMMAPPW ??
     public function set_reguls(string $jour, stdClass $reguls, string $tvset = "LFMMAPP") {
-        echo $jour." : ".$tvset."<br>";
         if ($tvset === "LFMMFMPE") $table = "reguls_est";
         if ($tvset === "LFMMFMPW") $table = "reguls_west";
         if ($tvset === "LFMMAPP") $table = "reguls_app";
@@ -700,7 +699,6 @@ class bdd {
             $constraints = $reg->constraints;
             $last_update = $reg->lastUpdate->userUpdateType;
             $last_update_time = substr($reg->lastUpdate->userUpdateEventTime,0,16);
-            echo $regId."<br>";
             $rates = [];
             foreach($constraints as $constraint) {
                 $rate = new stdClass();
